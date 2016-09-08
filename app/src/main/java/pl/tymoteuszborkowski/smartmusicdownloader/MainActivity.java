84b893361cf61d5a0cd585193987bb7b8d437d14
tymoteusz_borkowski <tborkovski@gmail.com>
2016-09-08 15:32:46 +0200

    add permission to >= 23 API , got file save to download directory

--- app/src/main/java/pl/tymoteuszborkowski/smartmusicdownloader/MainActivity.java
+++ app/src/main/java/pl/tymoteuszborkowski/smartmusicdownloader/MainActivity.java
@@ -1,5 +1,10 @@
 package pl.tymoteuszborkowski.smartmusicdownloader;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,20 +30,58 @@ public class MainActivity extends AppCompatActivity {
         titleEditText = (EditText) findViewById(R.id.titleEditText);
 
         button.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
                 final String artist = artistEditText.getText().toString();
                 final String title = titleEditText.getText().toString();
 
-                if((!artist.isEmpty()) && (!title.isEmpty())){
+                if ((!artist.isEmpty()) && (!title.isEmpty())) {
+                    if (shouldAskPermission()) {
+                        String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE"};
+                        if(!hasPermission(perms[0])){
+                            int permsRequestCode = 200;
+                            requestPermissions(perms, permsRequestCode);
+                        }
+
+                    }
                     passParameters = new PassParameters(artist, title);
                     Thread thread = new Thread(passParameters);
                     thread.start();
                 }
+
             }
         });
 
     }
 
 
+    private boolean shouldAskPermission() {
+
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+
+            case 200:
+
+                boolean writeAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                System.out.println("ACCEPTED: " + writeAccepted);
+                break;
+
+        }
+    }
+
+
+    @SuppressLint("NewApi")
+    private boolean hasPermission(String permission) {
+        boolean canMakeSmores = Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1;
+        return !canMakeSmores || (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+
+    }
 }
--- app/src/main/java/pl/tymoteuszborkowski/smartmusicdownloader/communication/PassParameters.java
+++ app/src/main/java/pl/tymoteuszborkowski/smartmusicdownloader/communication/PassParameters.java
@@ -1,16 +1,28 @@
 package pl.tymoteuszborkowski.smartmusicdownloader.communication;
 
+import android.os.Environment;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
 public class PassParameters implements Runnable {
 
 
-    private static final String PASS_PARAMETERS_URL = "http://10.0.2.2:8080/api/enter";
+    private static final String DOWNLOAD_MP3_URI = "http://10.0.2.2:8080/api/mp3";
 
     private String artist;
     private String title;
@@ -25,14 +37,31 @@ public class PassParameters implements Runnable {
     public void run() {
 
         final Client client = ClientBuilder.newClient();
-        final WebTarget target = client.target(PASS_PARAMETERS_URL);
+        final WebTarget target = client.target(DOWNLOAD_MP3_URI);
         final UriBuilder uriBuilder = target.getUriBuilder();
 
         uriBuilder.queryParam("title", title);
         uriBuilder.queryParam("artist", artist);
         final URI uri = uriBuilder.build();
         final WebTarget webTarget = client.target(uri);
-        webTarget.request().get();
+        Response response = webTarget.request().get();
+
+
+        if(response.getStatus() == 200){
+            InputStream inputStream = (InputStream) response.getEntity();
+            if (inputStream != null) {
+                String filename = response.getHeaderString("filename");
+                File downloadDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + filename);
+                try {
+                    FileUtils.copyInputStreamToFile(inputStream, downloadDir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }else{
+            //// TODO: 08.09.16 IMPLEMENT MESSAGE "wrong status response - server fault"
+        }
 
     }
 }
